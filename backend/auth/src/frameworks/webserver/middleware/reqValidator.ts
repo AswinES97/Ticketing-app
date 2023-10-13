be4e5fd6609e89ffc4244906312f588a93934074
aswin <aswinedassery@gmail.com>
2023-10-13 14:52:10 +0530

    password regex added to validate

--- backend/auth/src/frameworks/webserver/middleware/reqValidator.ts
+++ backend/auth/src/frameworks/webserver/middleware/reqValidator.ts
@@ -1,12 +1,25 @@
-import { type Request, type Response } from 'express'
+import { type NextFunction, type Request, type Response } from 'express'
+
+import { ReqValidationError } from '@ticket-common/common'
 import { body, validationResult } from 'express-validator'
 
 const reqValidator = {
-  emailValidator: () => body('email')
-    .isEmail(),
-  validatorFn: (req: Request, res: Response) => {
-    // const errors = validationResult(req)
-    // if(errors.isEmpty())
+  emailValidator: () => {
+    return body('email')
+      .isEmail()
+  },
+  passwordValidator: () => {
+    return body('password')
+      .trim()
+      .notEmpty()
+      .custom(value => {
+        return /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^*-]).{8,}$/.test(value)
+      })
+  },
+  validatorFn: (req: Request, res: Response, next: NextFunction) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) throw new ReqValidationError(errors.array())
+    next()
   }
 }
 
--- backend/auth/src/frameworks/webserver/routes/__test__/user.signup.test.ts
+++ backend/auth/src/frameworks/webserver/routes/__test__/user.signup.test.ts
@@ -4,5 +4,6 @@ import app from '../../server'
 it('Sample test for sigup', async () => {
   return await request(app)
     .post('/api/v1/user/signup/email')
+    .send({ email: 'aswinedassery@gmail.com', password: 'Aswines@123' })
     .expect(200)
 })
